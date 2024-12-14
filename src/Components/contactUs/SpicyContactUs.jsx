2d79ff9d1e2cfd3481d1e8e0edd2481413e154e8
Muhammad Ali <122118041+AleeDe@users.noreply.github.com>
2024-12-14 18:22:34 +0500

    Update SpicyContactUs.jsx

--- src/Components/contactUs/SpicyContactUs.jsx
+++ src/Components/contactUs/SpicyContactUs.jsx
@@ -28,7 +28,7 @@ export default function SpicyContactUs() {
         e.preventDefault();
         const token = localStorage.getItem('token');
         try {
-            const response = await axios.post('http://localhost:8080/contactus', formData, {
+            const response = await axios.post('http://54.221.19.62:8080/contactus', formData, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
