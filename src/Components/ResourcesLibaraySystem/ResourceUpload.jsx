5bd62c744c410ecba2d827e3f73a2a25908e6d7a
Muhammad Ali <122118041+AleeDe@users.noreply.github.com>
2024-12-15 01:33:47 +0500

    Update ResourceUpload.jsx

--- src/Components/ResourcesLibaraySystem/ResourceUpload.jsx
+++ src/Components/ResourcesLibaraySystem/ResourceUpload.jsx
@@ -51,7 +51,7 @@ const ResourceUpload = ({ refreshData }) => {
         const token = localStorage.getItem('token');
 
         try {
-            const response = await axios.post('http://44.223.145.60:8080/resources', formData, {
+            const response = await axios.post('http://54.166.196.136:8080/resources', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                     'Authorization': `Bearer ${token}`
