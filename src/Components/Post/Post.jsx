ebcba489ccc5477604a3edb209791bb2251ca85c
Muhammad Ali <122118041+AleeDe@users.noreply.github.com>
2024-12-14 18:21:19 +0500

    Update Post.jsx

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -21,7 +21,7 @@ export default function Post() {
     const fetchStudentData = useCallback(async () => {
         setLoading(true);
         try {
-            const response = await axios.get('http://localhost:8080/jiPost', {
+            const response = await axios.get('http://54.221.19.62:8080/jiPost', {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
